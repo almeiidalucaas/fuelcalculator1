fa0123479f0c3b8983a0da2bc85d6d6e1df011ec
almeiidalucaas <edson_elca15@hotmail.com>
2025-01-12 01:58:26 +0100

    add some editTextLayout and val

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.lucas.fuelcalculator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lucas.fuelcalculator"
--- app/src/main/java/com/lucas/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/lucas/fuelcalculator/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.lucas.fuelcalculator
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Recuperar os componentes editText
+        //criar variavel e associar o componente de UI (editText)
+
+        val edtPrice = findViewById<TextInputEditText>(R.id.fuel_price)
+        val edtConsume = findViewById<TextInputEditText>(R.id.car_consumption)
+        val edtDistance = findViewById<TextInputEditText>(R.id.distance)
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener {
+            val price = edtPrice.text
+            val consume = edtConsume.text
+            val distance = edtDistance.text
+            println("Lucas" + price)
+
         }
+
     }
 }
